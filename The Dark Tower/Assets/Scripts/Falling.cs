7a632c19f84ea47e412d3ca0c7b114c3ae46a393
SuperSpriteV <sufwan227@gmail.com>
2022-07-04 02:58:36 +0500

    Added Jump Animation
    Linked 3 parts of jump animations
    current task is to freeze pos x of player while final part of jump animation is playing

---
+++ The Dark Tower/Assets/Scripts/Falling.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Falling : MonoBehaviour
+{
+    public Rigidbody2D playerRigidBody2D;
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRigidBody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerRigidBody2D.velocity.y < 0){
+            animator.SetBool("Falling",true);
+        } else {
+            animator.SetBool("Falling",false);
+        }
+    }
+}
